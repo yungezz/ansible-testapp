a44859e16c7e1dcd762c9cc1953f4fd03fb4c65b
yungez <yungez@microsoft.com>
2018-06-26 11:13:47 +0800

    test

--- perfrunner.py
+++ perfrunner.py
@@ -6,6 +6,7 @@ import random
 import string
 import time
 import subprocess
+import csv
 
 from os.path import expanduser
 
@@ -29,7 +30,7 @@ class PerfRunner(object):
             index = index + 1
             for item in azcli_commands:
                 for command in item:
-                    print('start to run ' + command)
+                    print('start to run az cli ' + command)
                     # measure time start
                     start = time.time()
 
@@ -40,7 +41,7 @@ class PerfRunner(object):
                     end = time.time()
                     latency = end - start
 
-                    results[command] = latency
+                    results[command + index] = latency
                     print(command + "======================================")
                     print(latency)
             
@@ -54,6 +55,11 @@ class PerfRunner(object):
         
         print('test done!')
 
+        with open(self._args.output, 'wb') as file:
+            w = csv.DictWriter(file, results.keys())
+            w.writeheader()
+            w.writerow(results)
+
     def _parse_cli_args(self):
         parser = argparse.ArgumentParser(
                     description='Produce an Ansible Inventory file for an Azure subscription')
@@ -62,7 +68,7 @@ class PerfRunner(object):
                             help='test repeat times')
         parser.add_argument('--resource_group', action='store', default='ansibleperftest',
                             help='resource group name')
-        parser.add_argument('--output', action='store', default='.\testresult.txt',
+        parser.add_argument('--output', action='store', default='./azcli_output.txt',
                             help='output file name')
         return parser.parse_args()
 
@@ -75,9 +81,17 @@ class PerfRunner(object):
         result.append({ 'create_resourcegroup': "az group create -n {0} -l eastus".format(resourcegroup) })
         result.append({ 'create_vnet': "az network vnet create -g {0} -n {1} --address-prefix 10.0.0.0/16".format(resourcegroup, vm_name) })
         result.append({ 'create_subnet': "az network vnet subnet create -g {0} -n {1} --vnet-name {2} --address-prefix 10.0.0.0/24".format(resourcegroup, vm_name, vm_name) })
-        result.append({ 'create_vm': "az vm create -g {0} -n {1} --size Standard_DS1_v2 --vnet-name {2} --image UbuntuLTS".format(resourcegroup, vm_name, vm_name) })
+        result.append({ 'create_publicip': "az network public-ip create -g {0} -n {1} --allocation-method Static".format(resourcegroup, vm_name) })
+        result.append({ 'create_nsg': "az network nsg create -g {0} -n {1} --tags super_secure".format(resourcegroup, vm_name) })
+        result.append({ 'create_nic': "az network nic create -g {0} -n {1} --vnet-name {2} --subnet {3} --network-security-group {4} --public-ip-address {5}".format(resourcegroup, vm_name, vm_name, vm_name, vm_name) })
+        result.append({ 'create_vm': "az vm create -g {0} -n {1} --size Standard_DS1_v2 --nics {2} --image UbuntuLTS --os-disk-name {3}".format(resourcegroup, vm_name, vm_name, vm_name) })
         result.append({ 'update_vm': "az vm update -g {0} -n {1} --set tags.testTag=xxxx".format(resourcegroup, vm_name) })
         result.append({ 'delete_vm': "az vm delete -g {0} -n {1} -y".format(resourcegroup, vm_name) })
+        result.append({ 'delete_disk': "az disk delete -g {0} -n {1} -y".format(resourcegroup, vm_name) })
+        result.append({ 'delete_nic': "az network nic delete -g {0} -n {1} -y".format(resourcegroup, vm_name) })
+        # result.append({ 'delete_nsg': "az network nsg delete -g {0} -n {1} -y".format(resourcegroup, vm_name) })
+        result.append({ 'delete_publicip': "az network public-ip delete -g {0} -n {1} -y".format(resourcegroup, vm_name) })
+        result.append({ 'delete_resourcegroup': "az group delete -n {0}".format(resourcegroup) })
 
         return result
 
@@ -87,9 +101,9 @@ class PerfRunner(object):
 
         vm_name = 'vm' + seed
         playbook = "./playbooks/vm_create.yml"
-        logfile = "./ansible.output"
+        logfile = "./ansible_" + seed + ".output"
 
-        result.append({ 'ansiblevm': "ansible-playbook {0} --extra-vars resource_group={1} vm_name={2} >>  {3}".format(playbook, resourcegroup, vm_name, logfile) })
+        result.append({ 'ansiblevm': "ansible-playbook {0} --extra-vars \"resource_group={1} vm_name={2}\" >>  {3}".format(playbook, resourcegroup, vm_name, logfile) })
 
 def main():
     PerfRunner()
